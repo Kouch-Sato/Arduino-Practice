ecbd9b4e4e2b3a30aae39f0440bae790ee6acc5d
Kouch-Sato <redkouchred@gmail.com>
2025-11-28 23:29:29 +0900

    02

---
+++ Tutorial/02.ino
@@ -0,0 +1,12 @@
+void setup() {
+  pinMode(3, INPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop() {
+  if (digitalRead(3) == HIGH) {
+    digitalWrite(7, HIGH);
+  } else {
+    digitalWrite(7, LOW);
+  }
+}
